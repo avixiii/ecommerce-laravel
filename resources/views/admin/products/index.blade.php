c66ba1374f5e18bcb5bc9df043cb574b4a157aac
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-11-14 19:34:28 +0700

    feat(pagination): list products in dashboard

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -56,6 +56,7 @@
                             </tr>
                         @endforeach
                         </tbody>
+                        {{ $products->links() }}
                     </table>
                 </div>
             </div>
